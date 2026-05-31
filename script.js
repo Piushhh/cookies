21d588a3a5535c9658fda9498a691ad796d68402
Piushhh <piush02jha@gmail.com>
2026-05-31 23:06:00 +0530

    Add the final functionalities to the cookiecard

--- script.js
+++ script.js
@@ -4,11 +4,27 @@
 3. Also I need to accept the cookie information which the user provides how do I do that? so when the user visits frequently the pop-up doesn't appear again and again.
 4.When the HTML loads, the banner is visible by default. What is the very first check the JavaScript needs to run before the user even looks at the screen?
 */
-const cookieCard = document.getElementById("container")
-const cancelbtn = document.getElementById("cancel")
 
+
+
+//grab the elements
+const cookieCard = document.getElementById("container");
+const cancelbtn = document.getElementById("cancel");
+const acceptbtn = document.getElementById("accept");
+
+//Define close button function
 function hidePopUp(){
-   //hide the css property of this button
     cookieCard.style.display = 'none';
 }
-cancelbtn.addEventListener('click', hidePopUp)
+
+//when the user visits again the pop-up should not appear if they have already accepted cookies
+if (localStorage.getItem("cookieAccepted") === "true") {
+    hidePopUp();
+}
+
+//Attach  event listeners 
+cancelbtn.addEventListener('click', hidePopUp);
+acceptbtn.addEventListener('click', () => {
+    localStorage.setItem("cookieAccepted", "true");
+    hidePopUp();
+});
